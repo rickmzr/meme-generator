e18b3dda34e8ce9e3ddb4ee52566ece54791ae4f
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-06-19 21:39:07 -0500

    Add Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import troll from "./assets/TrollFace.png";
 import Header from "./components/Header";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
       <Header image={troll} />
+      <main>
+        <Form />
+      </main>
     </>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+export default function Form(props) {
+  return (
+    <form className="grid memeform">
+      <input type="text" />
+      <input type="text" />
+      <button className="submit-btn" type="submit">
+        Get a new meme image 🖼
+      </button>
+    </form>
+  );
+}
