de150dfdc7e80540103fe21d6cd1f3270cafa8ba
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-06-21 07:41:24 -0500

    Finish #6

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,23 @@
+import memesData from "../assets/memesData";
+
 export default function Form(props) {
+  const randomImage = (memesData) => {
+    let arrLength = memesData.data.memes.length;
+    let rndNum = Math.floor(Math.random() * arrLength);
+    return memesData.data.memes[rndNum].url;
+  };
+
   return (
-    <form className="grid memeform">
-      <input type="text" />
-      <input type="text" />
-      <button className="submit-btn" type="submit">
+    <div className="grid memeform">
+      <input type="text" placeholder="Top Text" />
+      <input type="text" placeholder="Bottom Text" />
+      <button
+        className="submit-btn"
+        type="submit"
+        onClick={() => console.log(randomImage(memesData))}
+      >
         Get a new meme image 🖼
       </button>
-    </form>
+    </div>
   );
 }
